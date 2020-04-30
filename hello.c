6844f0d2613dbf172d2b9cf9804e51edd404b414
ArkadiiMV <63120856+ArkadiiMV@users.noreply.github.com>
2020-04-30 08:58:30 +0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+//CS50 Problem Set 1, 2020: Hello
+//Author: ArkadiiMV
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+   string my_screen_message=get_string("Introduce yourself\n");
+   
+    printf("Hello, %s\n",my_screen_message);
+} 
